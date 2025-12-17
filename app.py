e7ffacae126e037b99dd3a2b7111841b335124b8
mostafaNasef-dev <mostafa.nasef2719@gmail.com>
2025-12-17 16:01:35 +0200

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,53 @@
 from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
+import psycopg2
+import pandas as pd
+import os
+
+def get_conn():
+    return psycopg2.connect(
+        host=os.environ["DB_HOST"],
+        database=os.environ["DB_NAME"],
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+        port=6543,
+        sslmode="require",
+    )
+
+def load_products():
+    with get_conn() as conn:
+        return pd.read_sql(
+            "SELECT * FROM products ORDER BY product_code",
+            conn
+        )
+
+def upsert_product(code, name, opening, capacity, cost):
+    with get_conn() as conn:
+        cur = conn.cursor()
+        cur.execute(
+            """
+            INSERT INTO products
+            (product_code, product_name, opening_inventory, monthly_capacity, unit_cost)
+            VALUES (%s,%s,%s,%s,%s)
+            ON CONFLICT (product_code)
+            DO UPDATE SET
+                product_name = EXCLUDED.product_name,
+                opening_inventory = EXCLUDED.opening_inventory,
+                monthly_capacity = EXCLUDED.monthly_capacity,
+                unit_cost = EXCLUDED.unit_cost
+            """,
+            (code, name, opening, capacity, cost)
+        )
+        conn.commit()
+
+def delete_product(code):
+    with get_conn() as conn:
+        cur = conn.cursor()
+        cur.execute(
+            "DELETE FROM products WHERE product_code = %s",
+            (code,)
+        )
+        conn.commit()
 
 # ---------------- App ----------------
 app = Dash(
